5e26a5e9ce4cd4b185c4addfb5b02124d11d1e83
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2024-03-03 05:39:02 +0800

    add files

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,33 +3,52 @@ import LiveServer from 'alive-server'
 import imagePlugin from 'esbuild-plugin-inline-image'
 import copy from 'esbuild-copy-static-files'
 
-const Server = ()=>{
-    const liveParams = { root: 'dist', port: 4650, host: 'localhost', file: 'index.html', fallback: 'index.html',  headers: {
-        'Content-Security-Policy': "default-src 'self'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://food-reservation.vercel.app/ http://localhost:4650"
-    }}
-    LiveServer.start(liveParams)
-}
+const generateNonce = () => {
+    const nonceLength = 32; 
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let nonce = '';
+    for (let i = 0; i < nonceLength; i++) {
+        nonce += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return nonce;
+};
+
+const Server = (nonce) => {
+    const liveParams = {
+        root: 'dist',
+        port: 4650,
+        host: 'localhost',
+        file: 'index.html',
+        fallback: 'index.html',
+        headers: {
+            'Content-Security-Policy': `default-src 'self'; script-src 'self' 'nonce-${nonce}'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://food-reservation.vercel.app/ sha256-<hFiMacGS/DT3jfBPaCojQ22ArBEbw5Nsd1jMxzuMLNE=>`
+        }
+    };
+    LiveServer.start(liveParams);
+};
+
 const EsbuildOptions = {
         entryPoints:['src/index.ts','index.html','src/**/*.ts'],
         outdir: 'dist',
         bundle: true,
+        allowOverwrite: true,
         minify: true,
         splitting: true,
         format: 'esm',
-        chunkNames: 'src/[name]-[hash]',
+        minifyWhitespace: true,
         // sourcemap: true,
         loader: {
             '.html':'copy',
-            '.png':'copy',
+            '.png':'file',
             '.css':'css',
             '.ts':'ts',
             '.woff': 'file',
             '.woff2': 'file',
             '.eot': 'file',
             '.ttf': 'file',
-            '.jpg': 'copy',
-            '.webp': 'copy',
-            '.avif': 'copy'
+            '.jpg': 'file',
+            '.webp': 'file',
+            '.avif': 'file'
         },
         external: ['/*.png','/*.jpg','/*.webp','/*.avif'],
         plugins: [
@@ -44,13 +63,19 @@ const EsbuildOptions = {
     }
 
     if(process.argv.includes('--build')){
+        const now = Date.now()
         await esbuild.build(EsbuildOptions).catch(() => {
             process.exit(1)
         });
+        const end = Date.now()
+        console.log(`build in ${((end - now) / 1000).toFixed(2)}`)
     } else {
+        const now = Date.now()
         const ctx = await esbuild.context(EsbuildOptions).catch(() => {
             process.exit(1)
         });
         ctx.watch()
         Server()
+        const end = Date.now()
+        console.log(`serve in ${((end - now) / 1000).toFixed(2)}`)
     }
--- src/pages/auth/pages/login/Login.ts
+++ src/pages/auth/pages/login/Login.ts
@@ -3,9 +3,31 @@ import './Login.css'
 export default function Login(DOM: HTMLDivElement) {
 
   DOM.innerHTML = (`
-    <div>
-      <h1>Login</h1>
+    <div class='login-page'>
+       <div class='login-content'>
+          <div class='login-left'>
+             <h2>Login</h2>
+                <form>
+                  <div class='inputs'>
+                    <label for='user' id='user'>Username</label>
+                    <input type='text' />
+                  </div>
+                  <div class='inputs'>
+                    <label for='pass' id='password'>Password</label>
+                    <input type='password' />
+                  </div>
+                  <button>Log in</button>
+                  <pre>This will be finish later :></pre>
+                </form>
+          </div>
+          <div class='login-right'>
+             <h2>Register</h2>
+          </div>
+       </div>
     </div>
   `);
+
+   const navChange = document.querySelector('nav') as HTMLDivElement
+   navChange.classList.add('text-white')
 
 }
