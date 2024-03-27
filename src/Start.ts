0859deee8c232015218832f15acad2d327fbfa22
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2024-03-28 01:33:48 +0800

    optimized redux

--- src/Start.ts
+++ src/Start.ts
@@ -1,13 +1,11 @@
-import Footer from "./components/sections/footer/Footer.ts"
-import Navbar from "./components/sections/navbar/Navbar.ts"
-import { fetchSliderData, fetchUserData } from "./redux/redux.state.ts"
-import store from "./redux/redux.state.ts"
-import { Routes } from "./utils/router/router.ts"
-import { scriptElement } from "./utils/sanitizer/domSanitizer.ts"
+import Footer from "./components/sections/footer/Footer.ts";
+import Navbar from "./components/sections/navbar/Navbar.ts";
+import { fetchSliderData, fetchUserData } from "./redux/redux.state.ts";
+import store from "./redux/redux.state.ts";
+import { Routes } from "./utils/router/router.ts";
+import { scriptElement } from "./utils/sanitizer/domSanitizer.ts";
 
 export default function Start(DOM: HTMLDivElement) {
-
-    // Function to fetch data
     const fetchData = async () => {
         try {
             await store.dispatch(fetchSliderData());
@@ -17,14 +15,13 @@ export default function Start(DOM: HTMLDivElement) {
         }
     };
 
-    // Subscribe to Redux store changes
     const handleStoreChange = () => {
         const state = store.getState();
         const sliderData = state.slider.sliderData;
         const userData = state.user.userData;
 
         const userSliderData = sliderData;
-        const [users] = userData;
+        const [users] = userData || [];
         const allusers = userData;
 
         const username = users ? users.name : 'Default Username';
@@ -33,36 +30,33 @@ export default function Start(DOM: HTMLDivElement) {
 
         const posts = userSliderData;
 
-        const user = { username, userRole, profile };
+        if (sliderData) {
+            DOM.innerHTML = (`
+                <div>
+                    <nav id='navbar'></nav>
+                    <div id='router'></div>
+                    <footer id='footer'></footer>
+                </div>
+            `);
 
-        DOM.innerHTML = (`
-            <div>
-                <nav id='navbar'></nav>
-                <div id='router'></div>
-                <footer id='footer'></footer>
-            </div>
-        `);
+            const navbar = document.getElementById('navbar') as HTMLDivElement;
+            navbar.appendChild(scriptElement);
+            Navbar(navbar);
 
-        const navbar = document.getElementById('navbar') as HTMLDivElement;
-        navbar.appendChild(scriptElement);
-        Navbar(navbar);
+            const routes = document.getElementById('router') as HTMLDivElement;
+            routes.appendChild(scriptElement);
+            Routes(routes, { username, userRole, profile }, posts, allusers);
 
-        const routes = document.getElementById('router') as HTMLDivElement;
-        routes.appendChild(scriptElement);
-        Routes(routes, user, posts, allusers);
-
-        const footer = document.getElementById('footer') as HTMLDivElement;
-        footer.appendChild(scriptElement);
-        Footer(footer);
+            const footer = document.getElementById('footer') as HTMLDivElement;
+            footer.appendChild(scriptElement);
+            Footer(footer);
+        }
     };
 
-    // Initial fetch
     fetchData();
 
-    // Subscribe to store changes
     const unsubscribe = store.subscribe(handleStoreChange);
 
-    // Unsubscribe from store changes when component unmounts
     return () => {
         unsubscribe();
     };
